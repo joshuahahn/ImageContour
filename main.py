cb3d82560650830e11b394398a23d7f6b5071067
joshuahahn <joshua.hahnjy@gmail.com>
2022-08-24 16:45:15 -0400

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    # Task: Given an array of floating point values, find an outline 
+    # such that an outline is defined by a difference in value
+    # greater than a user-defined threshold, k.
+    print("Hello World")
+
+
+
+if __name__ == "__main__":
+    main()
