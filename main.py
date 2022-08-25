84b8072b75c2437eb03a8b05a43f84690f505723
joshuahahn <joshua.hahnjy@gmail.com>
2022-08-25 10:56:20 -0400

    image import

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image, ImageOps
+import matplotlib
+import math
+
 def main():
     # Task: Given an array of floating point values, find an outline 
     # such that an outline is defined by a difference in value
     # greater than a user-defined threshold, k.
-    print("Hello World")
+
+    RGBImage = Image.open('panda.jpeg')
+    grayImage = ImageOps.grayscale(RGBImage)
+    imageArray = np.asarray(grayImage)
+
+    #plt.imshow(imageArray, cmap='gray')
+    #plt.show()
+
+    width = imageArray.shape[0]
+    height = imageArray.shape[1]
+
+    print(width)
+    print(height)
+
+
 
 
 
